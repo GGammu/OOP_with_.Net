dc33f26f46017d13195ef895adc3ef3af692c364
GGammu <ykcul02@gmail.com>
2014-05-29 18:41:57 +0530

    Fifth

---
+++ ThrowSystemException/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThrowSystemException
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            try
+            {
+                MessageBox.Show(numbers[9].ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Something went wrong:" + ex.Message);
+            }
+        }
+    }
+}
